fe63a2fd6244279cbe7ea6e33e852755e6be4c2d
kanishka424 <kanishkamalpana24@gmail.com>
2021-02-19 16:31:06 +0530

    using predicts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,19 +9,57 @@ public class Main {
         Employee tim = new Employee("Tim Buchalka", 21);
         Employee jack = new Employee("Jack Hill", 40);
         Employee snow = new Employee("Snow White", 22);
+        Employee charm = new Employee("Charm pizer", 32);
+
 
         List<Employee> employees = new ArrayList<>();
         employees.add(john);
         employees.add(tim);
         employees.add(jack);
         employees.add(snow);
+        employees.add(charm);
 
 
-        employees.forEach(employee->{//java 8 method in iterbale interface
-            System.out.println(employee.getName());
-            System.out.println(employee.getAge());
+
+
+
+
+//        System.out.println("Employees above age 32"); //ref A
+//        employees.forEach(employee->{
+//            if(employee.getAge()>30){
+//                System.out.println(employee.getName());
+//            }
+//        });
+//
+//
+//        System.out.println("Employees below age 32");//ref A
+//        employees.forEach(employee->{
+//            if(employee.getAge()<30){
+//                System.out.println(employee.getName());
+//            }
+//        });
+
+
+//
+//        employees.forEach(employee->{//java 8 method in iterbale interface
+//            System.out.println(employee.getName());
+//            System.out.println(employee.getAge());
+//        });
+
+        printEmployeeByAge(employees,"Employees older than 30",employee->employee.getAge()>30);
+        printEmployeeByAge(employees,"Employees younger than 30",employee->employee.getAge()>30);
+
+    }
+
+
+    public static void printEmployeeByAge(List<Employee> employees, String lineText, Predicate<Employee> ageCondition){//how to ue Predicate
+        System.out.println(lineText);
+        System.out.println("===============================");
+        employees.forEach(employee->{
+            if(ageCondition.test(employee)){
+                System.out.println(employee.getName());
+            }
         });
-
 
     }
 }
