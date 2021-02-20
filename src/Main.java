48be4a54c04f5bcbe86a88a37601da91f5d59085
kanishka424 <kanishkamalpana24@gmail.com>
2021-02-20 11:04:03 +0530

    Predicate and supplier

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
@@ -46,8 +49,33 @@ public class Main {
 //            System.out.println(employee.getAge());
 //        });
 
-        printEmployeeByAge(employees,"Employees older than 30",employee->employee.getAge()>30);
+        printEmployeeByAge(employees,"Employees older than 30",employee->employee.getAge()>30);//predicate with lambda
         printEmployeeByAge(employees,"Employees younger than 30",employee->employee.getAge()>30);
+        printEmployeeByAge(employees, "\n Employees younger than 25", new Predicate<Employee>() {//predicate with anonymous
+            @Override
+            public boolean test(Employee employee) {
+                    return employee.getAge()<25;
+            }
+    });
+
+
+        IntPredicate greatThan15=i->i>15;
+        IntPredicate lessThan10=i->i<10;
+        System.out.println("28 checked with great than 15\n"+greatThan15.test(28));
+        System.out.println("12 checked with great than 15\n"+greatThan15.test(12));
+        System.out.println("34 checked with less than 10\n"+ lessThan10.test(34));
+        System.out.println("4 checked with less than 10\n"+ lessThan10.test(4));
+
+
+        Random random =new Random();
+        Supplier<Integer> randomSupplier=()->random.nextInt(1000);//Supplier interface
+        for(int i=0;i<10;i++){
+            System.out.println(randomSupplier.get());
+        }
+
+
+
+
 
     }
 
@@ -63,3 +91,9 @@ public class Main {
 
     }
 }
+
+
+
+//NOTES
+//1.Predicate  is a one parameter lambda and it returns a boolean(we also can use anonymous classes to it)
+//2.from this we can infer type of the predicate "Predicate<Employee> ageCondition",use generics to infer type
